783dec1722d88292656ac70bd5b4e681ca12e057
HJKIM <guswns589898@gmail.com>
2022-11-06 21:38:26 +0900

    함수 분리완료

--- AutoTrainBooking/AutoTrainBooking.py
+++ AutoTrainBooking/AutoTrainBooking.py
@@ -1,86 +1,95 @@
 # -*- coding: utf-8 -*-
+import time
+from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
 
-# Webdriver 파일의 경로를 입력
-driver = webdriver.Chrome("C:\\Git\\AutoTrainBooking\\chromedriver.exe")  
-# 이동 페이지 경로
-driver.get('https://etk.srail.co.kr/cmc/01/selectLoginForm.do')
-
-# 페이지 다 뜰 때 까지 기다림
-driver.implicitly_wait(15) 
+def open_brower():
+    driver = webdriver.Chrome("C:\\Git\\AutoTrainBooking\\chromedriver.exe")
+    return driver
 
 
-# ID
-driver.find_element(By.ID, 'srchDvNm01').send_keys('2081265815') 
-# 비번
-driver.find_element(By.ID, 'hmpgPwdCphd01').send_keys("cltkgo1649!")
+def login(driver, login_id, login_psw):
+    driver.get('https://etk.srail.co.kr/cmc/01/selectLoginForm.do')
+    driver.implicitly_wait(15)
+    driver.find_element(By.ID, 'srchDvNm01').send_keys(str(login_id))
+    driver.find_element(By.ID, 'hmpgPwdCphd01').send_keys(str(login_psw))
+    driver.find_element(By.XPATH, '//*[@id="login-form"]/fieldset/div[1]/div[1]/div[2]/div/div[2]/input').click()
+    driver.implicitly_wait(5)
+    return driver
 
-driver.find_element(By.XPATH, '//*[@id="login-form"]/fieldset/div[1]/div[1]/div[2]/div/div[2]/input').click()
-driver.implicitly_wait(5)
 
-# 조회페이지 경로
-driver.get('https://etk.srail.kr/hpg/hra/01/selectScheduleList.do')
-driver.implicitly_wait(5)
+def search_train(driver, dpt_stn, arr_stn, dpt_dt, dpt_tm, num_trains_to_check=2, want_reserve=False):
+    is_booked = False # 예약 완료 되었는지 확인용
+    cnt_refresh = 0 # 새로고침 회수 기록
 
-# 출발지
-dep_stn = driver.find_element(By.ID, 'dptRsStnCdNm')
-dep_stn.clear() 
-dep_stn.send_keys("동탄")
+    driver.get('https://etk.srail.kr/hpg/hra/01/selectScheduleList.do') # 기차 조회 페이지로 이동
+    driver.implicitly_wait(5)
+    # 출발지/도착지/출발날짜/출발시간 입력
+    elm_dpt_stn = driver.find_element(By.ID, 'dptRsStnCdNm')
+    elm_dpt_stn.clear()
+    elm_dpt_stn.send_keys(dpt_stn) # 출발지
+    elm_arr_stn = driver.find_element(By.ID, 'arvRsStnCdNm')
+    elm_arr_stn.clear()
+    elm_arr_stn.send_keys(arr_stn) # 도착지
+    elm_dptDt = driver.find_element(By.ID, "dptDt")
+    driver.execute_script("arguments[0].setAttribute('style','display: True;')", elm_dptDt)
+    Select(driver.find_element(By.ID,"dptDt")).select_by_value(dpt_dt) # 출발날짜
+    elm_dptTm = driver.find_element(By.ID, "dptTm")
+    driver.execute_script("arguments[0].setAttribute('style','display: True;')", elm_dptTm)
+    Select(driver.find_element(By.ID, "dptTm")).select_by_visible_text(dpt_tm) # 출발시간
 
-# 도착지
-arr_stn = driver.find_element(By.ID, 'arvRsStnCdNm')
-arr_stn.clear()
-arr_stn.send_keys("부산")
+    print("기차를 조회합니다")
+    print(f"출발역:{dpt_stn} , 도착역:{arr_stn}\n날짜:{dpt_dt}, 시간: {dpt_tm}시 이후\n{num_trains_to_check}개의 기차 중 예약")
+    print(f"예약 대기 사용: {want_reserve}")
 
-elm_dptDt = driver.find_element(By.ID, "dptDt")
-driver.execute_script("arguments[0].setAttribute('style','display: True;')", elm_dptDt)
+    driver.find_element(By.XPATH, "//input[@value='조회하기']").click() # 조회하기 버튼 클릭
+    driver.implicitly_wait(5)
+    time.sleep(1)
 
-# 여기가 기차 선택창
-Select(driver.find_element(By.ID,"dptDt")).select_by_value("20221104")
+    while True:
+        for i in range(1, num_trains_to_check+1):
+            standard_seat = driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7)").text
+            reservation = driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(8)").text
 
-# 출발 시간
-elm_dptTm = driver.find_element(By.ID, "dptTm")
-driver.execute_script("arguments[0].setAttribute('style','display: True;')", elm_dptTm)
-Select(driver.find_element(By.ID, "dptTm")).select_by_visible_text("18")
+            if "예약하기" in standard_seat:
+                print("예약 가능 클릭")
+                driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7) > a").click()
+                driver.implicitly_wait(3)
 
-# 시간 조회
-driver.find_element(By.XPATH,"//input[@value='조회하기']").click()
-driver.implicitly_wait(5)
+                if driver.find_elements(By.ID, 'isFalseGotoMain'):
+                    is_booked = True
+                    print("예약 성공")
+                    break
+                else:
+                    print("잔여석 없음. 다시 검색")
+                    driver.back()  # 뒤로가기
+                    driver.implicitly_wait(5)
 
-# 
-reserved = False
+            if want_reserve:
+                if "신청하기" in reservation:
+                    print("예약 대기 완료")
+                    driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(8) > a").click()
+                    is_booked = True
+                    break
 
-while True:
-    for i in range(1, 3): #3개 기차만 확인. 
-        # 18시 42분행
-        # 18시 57분행
-        # 19시 17분행
-        standard_seat = driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7)").text
+        if not is_booked:
+            time.sleep(randint(2, 4)) #2~4초 랜덤으로 기다리기
 
-        if "예약하기" in standard_seat:
-            print("예약 가능")          
-            driver.find_element(By.XPATH, f"/html/body/div[1]/div[4]/div/div[3]/div[1]/form/fieldset/div[6]/table/tbody/tr[{i}]/td[7]/a/span").click()
-            reserved = True
+            # 다시 조회하기
+            submit = driver.find_element(By.XPATH, "//input[@value='조회하기']")
+            driver.execute_script("arguments[0].click();", submit)
+            cnt_refresh += 1
+            print(f"새로고침 {cnt_refresh}회")
+            driver.implicitly_wait(10)
+            time.sleep(0.5)
+        else:
             break
+    return driver
 
-    if not reserved:
-        # 2초 기다리기
-        sleep(2)
-        
-        # 다시 조회하기
-        submit = driver.find_element(By.XPATH, "//input[@value='조회하기']")
-        driver.execute_script("arguments[0].click();", submit)
-        print("새로고침")
 
-        driver.implicitly_wait(10)
-        sleep(1)
-    else:
-        break
-
-##이거 뭔지 모르겠는데 비쥬얼스튜디오로 코딩하면 Selenium 자꾸 꺼져서 추가함;;
-#while(True):
-#    pass
+if __name__ == "__main__":
+    driver = open_brower()
+    driver = login(driver, '2081265815', 'cltkgo1649!')
+    search_train(driver, "동탄", "부산", "20220115", "08") #기차 출발 시간은 반드시 짝수
